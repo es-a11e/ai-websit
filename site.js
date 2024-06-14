a731f8cb53796a7bafa71305fe5bb0b64fe2599b
es-a11e <a99705355@gmail.com>
2024-06-14 17:46:37 +0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$('#MassageDiv').delay(1000).fadeOut(1500);
+
+
+
+
+
+
+
+
+
+
